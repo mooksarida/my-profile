bbbe1bc9b7e893d9ea99715250d7a6c87ae305bd
mooksarida <mooksarida@gmail.com>
2020-08-28 23:11:28 +0700

    Updates

---
+++ precache-manifest.4320f0ea41ca1011eb6188765a11ba4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6d2d3d6a4c6fecb06de337b45df490",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "9ccf6c22ec6790bfbd45",
+    "url": "/my-profile/static/css/main.e3a87923.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/my-profile/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-profile/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ccf6c22ec6790bfbd45",
+    "url": "/my-profile/static/js/main.1e9b19e1.chunk.js"
+  },
+  {
+    "revision": "7913b382b769e50be9be",
+    "url": "/my-profile/static/js/runtime-main.1a31f5df.js"
+  }
+]);
